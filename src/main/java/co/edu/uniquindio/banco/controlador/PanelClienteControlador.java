4de810ec3bb2c5d2e40b19d3d6437727eb2b1bb1
Sharif Giraldo Obando <sharifgiraldo23@gmail.com>
2025-04-07 16:17:47 -0500

    panelCliente

--- src/main/java/co/edu/uniquindio/banco/controlador/PanelClienteControlador.java
+++ src/main/java/co/edu/uniquindio/banco/controlador/PanelClienteControlador.java
@@ -1,44 +1,74 @@
 package co.edu.uniquindio.banco.controlador;
 
+import co.edu.uniquindio.banco.modelo.entidades.BilleteraVirtual;
+import co.edu.uniquindio.banco.modelo.entidades.Transaccion;
+import co.edu.uniquindio.banco.modelo.enums.Categoria;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import java.net.URL;
+import java.time.format.DateTimeFormatter;
+import java.util.ResourceBundle;
 
-
-
-/**
- * Clase que se encarga de gestionar las acciones de la interfaz gráfica del panel del cliente.
- * @author Sharif Giraldo Obando
- * @author Juan Sebastián Hernández
- * @author Santiago Ospina Sánchez
- */
-public class PanelClienteControlador {
+public class PanelClienteControlador implements Initializable {
 
     public Button btnCerrarSesion;
+    public Button btnConsultar;
+    public Button btnTransferir;
+    public Button btnActualizar;
 
+    public TableView<Transaccion> tblPanelCliente;
+    public TableColumn<Transaccion, String> colTipo;
+    public TableColumn<Transaccion, String> colFecha;
+    public TableColumn<Transaccion, Float> colValor;
+    public TableColumn<Transaccion, String> colUsuario;
+    public TableColumn<Transaccion, Categoria> colCategoria;
 
+    public Text cuentaTxt;
+    public Text bienvenidaClienteTxt;
 
+    private BilleteraVirtual billeteraActual;
 
-    /**
-     * Método que se encarga de cerrar sesión al oprimir el botón para culminar la
-     * sesión del usuario
-     * @param event
-     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        colTipo.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleStringProperty(cellData.getValue().getTipo().name()));
+        colFecha.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleStringProperty(cellData.getValue().getFecha().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"))));
+        colValor.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleFloatProperty(cellData.getValue().getMonto()).asObject());
+        colUsuario.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleStringProperty(cellData.getValue().getBilleteraOrigen().getUsuario().getNombre()));
+        colCategoria.setCellValueFactory(cellData ->
+                new javafx.beans.property.SimpleObjectProperty<>(cellData.getValue().getTipo()));
+    }
+
+    public void setBilleteraActual(BilleteraVirtual billetera) {
+        this.billeteraActual = billetera;
+
+        if (billetera != null) {
+            cuentaTxt.setText("Nro. de Cuenta : " + billetera.getNumero());
+            bienvenidaClienteTxt.setText("Bienvenido " + billetera.getUsuario().getNombre());
+            cargarTransacciones();
+        }
+    }
+
     public void cerrarSesionAction(ActionEvent event) {
         try {
-
             Stage stageActual = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stageActual.close();
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/inicio.fxml"));
             Parent root = loader.load();
 
-
             Stage nuevoStage = new Stage();
             nuevoStage.setScene(new Scene(root));
             nuevoStage.setTitle("Inicio");
@@ -47,35 +77,38 @@ public class PanelClienteControlador {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     public void consultarAction(ActionEvent event) {
+        cargarTransacciones();
+    }
+
+    private void cargarTransacciones() {
+        if (billeteraActual != null) {
+            ObservableList<Transaccion> lista = FXCollections.observableArrayList(billeteraActual.obtenerTransacciones());
+            tblPanelCliente.setItems(lista);
+        }
     }
 
     public void transferirAction(ActionEvent event) {
         try {
-
             Stage stageActual = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stageActual.close();
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/transferencia.fxml"));
             Parent root = loader.load();
 
-
             Stage nuevoStage = new Stage();
             nuevoStage.setScene(new Scene(root));
-            nuevoStage.setTitle("Inicio");
+            nuevoStage.setTitle("Transferencia");
             nuevoStage.show();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-
 
     public void actualizarAction(ActionEvent event) {
 
-    }
 
+    }
 }
