248d861f310baa64bd0384d614b1e2562a4327fd
Sharif Giraldo Obando <sharifgiraldo23@gmail.com>
2025-04-07 15:26:47 -0500

    Update TransferenciaControlador.java

--- src/main/java/co/edu/uniquindio/banco/controlador/TransferenciaControlador.java
+++ src/main/java/co/edu/uniquindio/banco/controlador/TransferenciaControlador.java
@@ -1,12 +1,35 @@
 package co.edu.uniquindio.banco.controlador;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Clase que se encarga de controlar la creación de transferencias entre billeteras
  * @author caflorezvi
  */
-public class TransferenciaControlador {
+public class TransferenciaControlador implements Initializable {
+
+
+
+    @FXML
+    private ComboBox<String> boxCategoria;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        boxCategoria.getItems().addAll(
+                "VIAJES",
+                "FACTURAS",
+                "GASOLINA",
+                "ROPA",
+                "PAGO",
+                "OTROS"
+        );
+    }
 
 
     public void transferirActionBtn(ActionEvent event) {
