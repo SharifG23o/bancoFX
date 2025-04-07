39a85e0ce2dda2aab169b1b1e53341cf89c8e019
JuanHG07 <juan.hernandezg@uqvirtual.edu.co>
2025-04-07 16:03:51 -0500

    Create Sesion.java

---
+++ src/main/java/co/edu/uniquindio/banco/modelo/Sesion.java
@@ -0,0 +1,29 @@
+package co.edu.uniquindio.banco.modelo;
+
+
+import co.edu.uniquindio.banco.modelo.entidades.Usuario;
+import lombok.Getter;
+import lombok.Setter;
+
+
+public class Sesion {
+
+    public static Sesion INSTANCIA;
+
+    @Getter @Setter
+    private Usuario usuario;
+
+    private Sesion() {
+    }
+
+    public static Sesion getInstancia() {
+        if (INSTANCIA == null) {
+            INSTANCIA = new Sesion();
+        }
+        return INSTANCIA;
+    }
+
+    public void cerrarSesion() {
+        usuario = null;
+    }
+}
